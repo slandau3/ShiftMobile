d349faad70904fd1b66b99a530e23f407faa0da9
Steven Landau <skl8881@g.rit.edu>
2016-10-09 19:07:54 -0400

    Initial commit

---
+++ app/src/main/java/edu/rit/cs/steven_landau/shiftmobile/TextReceiver.java
@@ -0,0 +1,57 @@
+package edu.rit.cs.steven_landau.shiftmobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import edu.rit.cs.steven_landau.shiftmobile.MainActivity;
+
+/**
+ * Created by Steven Landau on 9/27/2016.
+ */
+public class TextReceiver extends BroadcastReceiver{
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    public String thoughtMessage;
+
+
+
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(SMS_RECEIVED) || intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            Bundle intentExtras = intent.getExtras();
+            if (intentExtras != null) {
+                Object[] sms = (Object[]) intentExtras.get("pdus");
+                assert sms != null;
+                for (int i = 0; i < sms.length; ++i) {
+                    String format = intentExtras.getString("format");
+                    SmsMessage smsMessage = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        smsMessage = SmsMessage.createFromPdu((byte[]) sms[i], format);
+                    }
+
+                    assert smsMessage != null;
+                    String contents = smsMessage.getMessageBody();
+                    String sender = smsMessage.getOriginatingAddress();
+
+
+                }
+            }
+        }
+    }
+
+
+    public void sendMessage(String number, String msg) throws InterruptedException {
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(number, null, msg, null, null);
+    }
+}
